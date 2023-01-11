b3a7eb0679ae5169f7634a4429ab359d4ed55bce
Guilherme <guivoleckk@hotmail.com>
2023-01-11 19:16:41 -0300

    refatorando e arrumando bug

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-//pegando elementos
+// pegando elementos
 
 const pegandoPaletaCores = document.getElementById('color-palette');
 const pegandoLousaTamanho = document.querySelector('#pixel-board');
-const pegandoBotaoLimpar = document.getElementById('clear-board');
-const corInicial = document.querySelector('.color');
-const corSelecionada = document.getElementsByClassName('selected');
-
-
 let pegandoQuadradinhosPaleta = document.getElementsByClassName('color');
-
-const corInicialSelecionada = document.getElementsByClassName('selected');
-
 const pegaVqv = document.querySelector('#generate-board');
 
 const valorInput = document.querySelector('#board-size');
-const botaoVqv = document.querySelector
-('#generate-board');
+// funcoes
 
-const divizinhas = document.querySelectorAll('.pixel');
-
-//funcoes
-
-//defindo valor inicial matriz
-
-
+// defindo valor inicial matriz
 const construindoPaleta = () => {
-  for (let index = 0; index < 4; index +=1){
+  for (let index = 0; index < 4; index += 1) {
     const criaDiv = document.createElement('div');
     criaDiv.className = 'color';
     criaDiv.style.border = '1px solid black';
@@ -35,83 +20,66 @@ const construindoPaleta = () => {
     criaDiv.style.margin = '8px';
     pegandoPaletaCores.appendChild(criaDiv);
   }
-
-}
+};
 
 const pintandoPaleta = () => {
   const primeiroParam = Math.floor(Math.random() * 254);
   const segundoParam = Math.floor(Math.random() * 254);
   const terceiroiroParam = Math.floor(Math.random() * 254);
-    console.log(`rgb ${primeiroParam}, ${segundoParam}, ${terceiroiroParam}`)
-    return `rgb(${primeiroParam}, ${segundoParam}, ${terceiroiroParam})`;
-
-}
-
-
-const jogandoCoresPaleta = () => {
-  const pegandoQuadradinhosPaleta = document.getElementsByClassName('color');
-  for (let index = 1; index < pegandoQuadradinhosPaleta.length; index += 1){
-  pegandoQuadradinhosPaleta[0].style.backgroundColor = 'black';  
-  pegandoQuadradinhosPaleta[index].style.
-  backgroundColor = pintandoPaleta();  
-  
-  }
-  guardandoCoresAleatoriasStorage()
-}
-
-const gerandoCoresAleatorias = () => {
-  const pegandoBotao = document.getElementById('button-random-color');
-  pegandoBotao.addEventListener('click', () => {
-    pegandoQuadradinhosPaleta = jogandoCoresPaleta();
-  })
-
-}
+  console.log(`rgb ${primeiroParam}, ${segundoParam}, ${terceiroiroParam}`);
+  return `rgb(${primeiroParam}, ${segundoParam}, ${terceiroiroParam})`;
+};
 
 const guardandoCoresAleatoriasStorage = () => {
   const pegandoCoresQuadradinhos = document.getElementsByClassName('color');
   const salvandoCoresProStorage = [];
-  for (let index = 0; index < pegandoCoresQuadradinhos.length; index +=1){
+  for (let index = 0; index < pegandoCoresQuadradinhos.length; index += 1) {
     salvandoCoresProStorage.push(pegandoCoresQuadradinhos[index].style.backgroundColor);
   }
   localStorage.setItem('colorPalette', JSON.stringify(salvandoCoresProStorage));
-}
+};
+
+const jogandoCoresPaleta = () => {
+  for (let index = 1; index < pegandoQuadradinhosPaleta.length; index += 1) {
+    pegandoQuadradinhosPaleta[0].style.backgroundColor = 'black';
+    pegandoQuadradinhosPaleta[index].style.backgroundColor = pintandoPaleta();
+  }
+  guardandoCoresAleatoriasStorage();
+};
+
+const gerandoCoresAleatorias = () => {
+  const pegandoBotao = document.getElementById('button-random-color');
+  pegandoBotao.addEventListener('click', () => {
+    jogandoCoresPaleta();
+  });
+};
 
 const pintandoCoresSalvasStorage = () => {
   const pegandoOsquadradinhosDeNovo = document.getElementsByClassName('color');
   const recuperandoCoresStorage = JSON.parse(localStorage.getItem('colorPalette'));
 
-  for (let index = 0; index < pegandoOsquadradinhosDeNovo.length; index+=1){
+  for (let index = 0; index < pegandoOsquadradinhosDeNovo.length; index += 1) {
     pegandoOsquadradinhosDeNovo[index].style.backgroundColor = recuperandoCoresStorage[index];
-  }
-}
-
-
-
-const pretoNoInicio = () => {
-  const pegandoQuadradinhosPaleta = document.getElementsByClassName('color');
-  for (let index = 1; index < 
-  pegandoQuadradinhosPaleta.length; index += 1){
-    pegandoQuadradinhosPaleta[0].classList = 'color selected'; 
-}
-}
-
-
-
-const pegandoSelected = document.getElementsByClassName('selected');
-
-
-let selecionandoCor = () => {
-  const pegandoSelected = document.getElementsByClassName('selected');
-  for (let index = 0; index < pegandoQuadradinhosPaleta.length; index += 1){
-    pegandoQuadradinhosPaleta[index].addEventListener('click', () => {
-
-      pegandoSelected[0].classList.remove('selected');
-      pegandoQuadradinhosPaleta[index].classList.add('selected');
-    })
- 
   }
 };
 
+const pretoNoInicio = () => {
+  for (let index = 1; index < pegandoQuadradinhosPaleta.length; index += 1) {
+    pegandoQuadradinhosPaleta[0].classList = 'color selected';
+  }
+};
+
+const selecionandoCor = () => {
+  for (let index = 0; index < pegandoQuadradinhosPaleta.length; index += 1) {
+    pegandoQuadradinhosPaleta[index].addEventListener('click', (event) => {
+      const selecionado = document.querySelector('.selected');
+      if (selecionado) {
+        selecionado.classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    });
+  }
+};
 
 const pintarBoard = (event) => {
   if (event.target.classList.contains('pixel')) {
@@ -122,58 +90,54 @@ const pintarBoard = (event) => {
     event.target.style.backgroundColor = pegaBckgcolor;
   }
 };
+
 document.addEventListener('click', pintarBoard);
 const pegarBoard = document.getElementsByClassName('pixel');
-const botaoLimpar = document.querySelector('#clear-board')
+const botaoLimpar = document.querySelector('#clear-board');
 const limpaBoard = () => {
-  for (let index = 0; index < pegarBoard.length; index += 1){
+  for (let index = 0; index < pegarBoard.length; index += 1) {
     pegarBoard[index].style.backgroundColor = 'white';
   }
 };
 botaoLimpar.addEventListener('click', limpaBoard);
 
-
 const salvandoDesenho = () => {
   const pegandoOTalDesenho = document.querySelectorAll('.pixel');
   const arrayDesenhoSalvo = [];
-  for (let index = 0; index < pegandoOTalDesenho.length; index += 1){
+  for (let index = 0; index < pegandoOTalDesenho.length; index += 1) {
     arrayDesenhoSalvo.push(pegandoOTalDesenho[index].style.backgroundColor);
     localStorage.setItem('pixelBoard', JSON.stringify(arrayDesenhoSalvo));
   }
-  
 };
-
-
-
 
 const pintandoDesenhoSalvo = () => {
   const pegandoOTalDesenho = document.querySelectorAll('.pixel');
-  for (let index = 0; index < pegandoOTalDesenho.length; index += 1){
+  for (let index = 0; index < pegandoOTalDesenho.length; index += 1) {
     pegandoOTalDesenho[index].addEventListener('click', () => {
       const quadradoPintado = document.querySelector('.selected');
       pegandoOTalDesenho[index].style.backgroundColor = quadradoPintado.style.backgroundColor;
       salvandoDesenho();
-    })
+    });
   }
 };
 
 const jogarDesenhoTelaBranca = () => {
   const pegandoOTalDesenho = document.querySelectorAll('.pixel');
   const resgatandoStorageBckColor = JSON.parse(localStorage.getItem('pixelBoard'));
-  for (let index = 0; index < pegandoOTalDesenho.length; index += 1){
+  for (let index = 0; index < pegandoOTalDesenho.length; index += 1) {
     pegandoOTalDesenho[index].style.backgroundColor = resgatandoStorageBckColor[index];
   }
 };
 
-//tive que refatorar tudo que eu tinha feito, mesmo os testes passando nos req 13 e 14, pois tive dificuldade para pegar o length do input dinamicamente e salvar o tamanho do board, por conta disso tive q refazer esses requisitos para finalmente tentar resolver o req 15. 
+// tive que refatorar tudo que eu tinha feito, mesmo os testes passando nos req 13 e 14, pois tive dificuldade para pegar o length do input dinamicamente e salvar o tamanho do board, por conta disso tive q refazer esses requisitos para finalmente tentar resolver o req 15.
 
 const regularizaInput = () => {
   valorInput.type = 'number';
   valorInput.setAttribute('min', '1');
   valorInput.value = '';
-}
+};
 
-//só um teste para ver o dinamismo do numero no localStorage
+// só um teste para ver o dinamismo do numero no localStorage
 const roubandoNumeroLocalStorage = JSON.parse(localStorage.getItem('boardSize'))
 console.log(roubandoNumeroLocalStorage);
 
@@ -185,7 +149,6 @@ const criacaoTamanhoBoard = () => {
   } else if (valorInput.value > 50) {
     valorInput.value = 50;
   }
- 
   for (let index = 0; index < valorInput.value * valorInput.value; index += 1) {
     const divs = document.createElement('div');
     divs.classList.add('pixel');
@@ -193,16 +156,13 @@ const criacaoTamanhoBoard = () => {
     divs.style.backgroundColor = 'white';
     divs.style.width = '40px'; 
     divs.style.height = '40px';
-    divs.style.border = '1px solid black';
-    divs.style.margin = '8px';  
+    divs.style.border = '1px solid black'; 
     divs.style.display = 'inline-block'
-    pegandoLousaTamanho.style.height = 60 * valorInput.value + 'px';
-    pegandoLousaTamanho.style.width = 60 * valorInput.value + 'px';
+    pegandoLousaTamanho.style.height = 44 * valorInput.value + 'px';
+    pegandoLousaTamanho.style.width = 44 * valorInput.value + 'px';
   }
   valorInput.value = '';
-}
-
-
+};
 
 const chamaCriacaoBoard = () => {
   const lousa = document.getElementById('pixel-board');
@@ -213,43 +173,31 @@ const chamaCriacaoBoard = () => {
     localStorage.setItem('boardSize', JSON.stringify(valorInput.value));
     criacaoTamanhoBoard();
   }
-}
-
-const clicandoBotaoVqv = () => {
-    pegaVqv.addEventListener('click', chamaCriacaoBoard);
-}
-
-
-//carregando funcoes
-
-window.onload = () => {
-regularizaInput();
-
-chamaCriacaoBoard();
-criacaoTamanhoBoard();
-
-construindoPaleta();
-gerandoCoresAleatorias();
-
-if (localStorage.getItem('colorPalette') === null) {
-  jogandoCoresPaleta();
-} else {
-  pintandoCoresSalvasStorage();
-}
-
-clicandoBotaoVqv();
-
-
-pretoNoInicio();
-selecionandoCor();
-
-if (localStorage.getItem('pixelBoard') === null){
-  pintandoDesenhoSalvo();
-} else {
-  jogarDesenhoTelaBranca();
-}
-
-
-
 };
 
+const clicandoBotaoVqv = () => {
+  pegaVqv.addEventListener('click', chamaCriacaoBoard);
+};
+
+// carregando funcoes
+
+window.onload = () => {
+  regularizaInput();
+  chamaCriacaoBoard();
+  criacaoTamanhoBoard();
+  construindoPaleta();
+  gerandoCoresAleatorias();
+  if (localStorage.getItem('colorPalette') === null) {
+    jogandoCoresPaleta();
+  } else {
+    pintandoCoresSalvasStorage();
+  }
+  clicandoBotaoVqv();
+  pretoNoInicio();
+  selecionandoCor();
+  if (localStorage.getItem('pixelBoard') === null) {
+    pintandoDesenhoSalvo();
+  } else {
+    jogarDesenhoTelaBranca();
+  }
+};
